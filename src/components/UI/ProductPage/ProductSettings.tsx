f6beb817f39d534799bc9818bd7ec8f525d80b16
Vladyslav Zinchenko <vladyslav.zinchenko@gmail.com>
2024-10-24 21:20:32 +0300

    added cart and favourite logic

--- src/components/UI/ProductPage/ProductSettings.tsx
+++ src/components/UI/ProductPage/ProductSettings.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { FC } from "react";
+import { FC, useState } from "react";
 import SizeGrid from "@/components/common/SizeGrid";
 import CustomBtn from "@/components/common/CustomBtn";
 import ColorSelect from "@/components/common/ColorSelect";
@@ -21,8 +21,40 @@ const ProductSettings: FC<ProductSettingsProps> = ({
   setSelectedColor,
   sizes,
 }) => {
-  const addToCart = (id: number) => {
-    console.log(`Product with id:${id} added to the cart`);
+  const [selectedSize, setSelectedSize] = useState<string>("");
+
+  const [isInCart, setIsInCart] = useState(false);
+  const [isInFavorites, setIsInFavorites] = useState(false);
+
+  const addToCart = () => {
+    if (!selectedSize || !selectedColor) {
+      console.log("Please select size and colour");
+      return;
+    }
+
+    const productDetails = {
+      title,
+      category,
+      price,
+      selectedColor,
+      selectedSize,
+    };
+
+    console.log("Added to cart:", productDetails);
+    setIsInCart(true);
+  };
+
+  const addToFavorites = () => {
+    const productDetails = {
+      title,
+      category,
+      price,
+      selectedColor,
+      selectedSize,
+    };
+
+    console.log("Added to favourites:", productDetails);
+    setIsInFavorites(true);
   };
 
   return (
@@ -32,24 +64,39 @@ const ProductSettings: FC<ProductSettingsProps> = ({
         <h4 className="text-gray-500">{category}</h4>
         <p className="font-bold">${price}</p>
       </div>
+
       <ColorSelect
         selectedColor={selectedColor}
         setSelectedColor={setSelectedColor}
       />
 
-      <SizeGrid sizes={sizes} />
+      <SizeGrid
+        sizes={sizes}
+        selectedSize={selectedSize}
+        setSelectedSize={setSelectedSize}
+      />
+
       <div className="flex flex-col space-y-3">
         <CustomBtn
-          className="bg-black text-white duration-500 hover:bg-white hover:text-gray-500 border-[2px] border-gray-500"
-          onClick={() => addToCart(1)}
+          className={`${
+            isInCart
+              ? "bg-green-500 text-white border-green-500"
+              : "bg-black text-white border-white hover:bg-white hover:text-gray-500"
+          } duration-500 border-[2px] hover:border-gray-500`}
+          onClick={addToCart}
         >
-          <p>Add to bag</p>
+          <p>{isInCart ? "In the cart" : "Add to cart"}</p>
         </CustomBtn>
+
         <CustomBtn
-          className="border-[2px] border-black text-black duration-500 hover:bg-black hover:text-white"
-          onClick={() => addToCart(1)}
+          className={`border-[2px] duration-500 ${
+            isInFavorites
+              ? "bg-red-500 text-white border-red-500"
+              : "border-black text-black hover:bg-gray-500 hover:border-gray-500 hover:text-white"
+          }`}
+          onClick={addToFavorites}
         >
-          <p>Add to favourite</p>
+          <p>{isInFavorites ? "Favourites" : "Add to favourites"}</p>
         </CustomBtn>
       </div>
     </div>
--- src/components/common/SizeItem/index.tsx
+++ src/components/common/SizeItem/index.tsx
@@ -2,11 +2,21 @@ import { FC } from "react";
 
 interface SizeItemProps {
   size: string;
+  isSelected: boolean;
+  onClick: () => void;
 }
 
-const SizeItem: FC<SizeItemProps> = ({ size }) => {
+const SizeItem: FC<SizeItemProps> = ({ size, isSelected, onClick }) => {
   return (
-    <div className="w-[86px] h-[46px] duration-500 cursor-pointer bordered border-[1px] flex justify-center items-center hover:border-[2px] hover:border-black">
+    <div
+      onClick={onClick}
+      className={`w-[86px] h-[46px] cursor-pointer border-[1px] flex justify-center items-center duration-500 
+        ${
+          isSelected
+            ? "border-[2px] border-black font-bold"
+            : "hover:border-[2px] hover:border-black"
+        }`}
+    >
       <p>{size}</p>
     </div>
   );
